9c1ff1bd39e4060f2b7306b49d823d282af76298
MalindaCarisch <malinda.carisch@gmail.com>
2022-11-01 20:13:50 -0500

    changes to css

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ if ($(this).hasClass("tab1")) {
 $(".tab1").addClass("active");
 $(".tab2").removeClass("active");
 $("#tabContent1").css("display", "block");
-$("#tabContent2").css("display", "none");
+$("#tabContent2").addClass("hidden")
 }
 else if ($(this).hasClass("tab2")) {
 $(".tab2").addClass("active");
